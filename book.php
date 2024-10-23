a138b8d099aec6608e3ef9ca19034cf91f9daf84
Andrus <andrus.naulainen@ametikool.ee>
2024-10-23 11:50:32 +0300

    first commit

---
+++ book.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('./connection.php');
+
+$id = $_GET['id'];
+
+$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+$stmt->execute(['id' => $id]);
+$book = $stmt->fetch();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <h1>
+            <?= $book['title']; ?>
+        </h1>
+</body>
+</html>
